534f40c2d0fa1cae9462c7309dcc2a5004a28e7f
Fira <firaafx@gmail.com>
2026-02-09 16:52:34 +0300

    v0.04 build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "version.json": "bbbf8b47ea7f7450f76ef8f73078167b",
-"flutter_bootstrap.js": "d793803a147ecea4e7e9cf5d893ed929",
+"flutter_bootstrap.js": "ffb10df6b57acb10fab8d8e0e6eb2035",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
@@ -35,7 +35,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "index.html": "fff4ebc89b96c40145779e5a0b142f7c",
 "/": "fff4ebc89b96c40145779e5a0b142f7c",
-"main.dart.js": "5b09e8d30e78be05f278d77905ec9756"};
+"main.dart.js": "0ed37c26d7dc25ce2c95084ae8fe6896"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
